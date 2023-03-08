03c301fa3704f7534c1bea5ee55dc1203fc9dc44
Kavya Udalagama <udalagamak@ad.macewan.ca>
2023-03-07 19:04:25 -0700

    Form Validation changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
-// Wait for the DOM to be ready
+
 $(function() {
-  // Initialize form validation on the registration form.
-  // It has the name attribute "registration"
-  $("form[name='registration']").validate({
-    // Specify validation rules
+  $("form[name='contact']").validate({
     rules: {
-      // The key name on the left side is the name attribute
-      // of an input field. Validation rules are defined
-      // on the right side
       firstname: "required",
       lastname: "required",
       email: {
         required: true,
-        // Specify that email should be validated
-        // by the built-in "email" rule
         email: true
       },
-      password: {
-        required: true,
-        minlength: 5
-      }
     },
     // Specify validation error messages
     messages: {
@@ -31,8 +19,6 @@ $(function() {
       },
       email: "Please enter a valid email address"
     },
-    // Make sure the form is submitted to the destination defined
-    // in the "action" attribute of the form when valid
     submitHandler: function(form) {
       form.submit();
     }
